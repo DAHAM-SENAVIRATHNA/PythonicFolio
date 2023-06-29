ee94ca06a85bca7563e3455b50a09b183be3be5d
DAHAM-SENAVIRATHNA <nayanads2000@gmail.com>
2023-06-29 12:06:10 +0530

    add columns and profile img

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown(
+        """
+        <style>
+        img {
+            border-radius: 50%;
+            object-fit: cover;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.image("images/profile.png", width=450)
+with col2:
+    st.title("Nayana Senavirathna")
+
