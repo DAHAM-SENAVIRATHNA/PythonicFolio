ff92f3f93f9804c33c72f62ad5d2a4a2a314aeb8
DAHAM-SENAVIRATHNA <nayanads2000@gmail.com>
2023-06-30 15:20:15 +0530

    send email using form completed

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 
 st.header("Contact Me")
@@ -13,3 +14,7 @@ From: {user_email}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
+    print((button))
+    if button:
+        send_email(message)
+        st.info("Your email was sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,14 +1,16 @@
-
-import smtplib, ssl
 import os
+import smtplib
+import ssl
 
 
+# Send email and secure username/ password using to send email by env
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = os.getenv("E_USERNAME")
     password = os.getenv("PASSWORD")
 
+    # Add any email to receive the email from contact me form
     receiver = "nayanadsem@gmail.com"
     context = ssl.create_default_context()
 
